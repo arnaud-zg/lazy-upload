e436e3dc3f1306438152cf143a8d2fbbb2ad1512
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2020-03-09 13:56:50 +0100

    fix: add with validation story

---
+++ playground/stories/Hooks/FileUpload/WithValidation/index.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Layout } from "../../../../src/common/Layout";
+import { Title } from "../../../../src/common/Title";
+import { Box } from "../../../../src/common/Box";
+
+export const FileUpload = () => (
+  <Layout>
+    <Title>File upload</Title>
+    <Box>Coming soon</Box>
+  </Layout>
+);
+
+export default {
+  title: "Hooks/File Upload/With Validation",
+  component: FileUpload
+};
